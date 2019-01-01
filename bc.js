bca8011c54db1d39f8b337156f8de52e512442a0
7vvks <45107550+7vvks@users.noreply.github.com>
2019-01-01 15:57:03 +0100

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,29 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+var prefix = "$";
+
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "obc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+}); 
+
+
+
+
+
+ client.login("NTI5MzYwNjE3NjMxNTE0NjI1.DwvtPg.nJoSKghEhcD-MzMe5gPqgy6eogo");
